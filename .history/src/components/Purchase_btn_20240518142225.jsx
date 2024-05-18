bd7c8716d4d518a43c8295f0cdeac31a90a69b34
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-05-18 14:24:47 +0530

    finally done compeletly (fixed one bug)

---
+++ .history/src/components/Purchase_btn_20240518142225.jsx
@@ -0,0 +1,26 @@
+import Magic_stick from '../assets/motionarteffect-img5.png'
+import { GoPaperAirplane } from "react-icons/go";
+function Purchase(){
+    return(
+        <>
+        <section className='Purchase flex md:flex-row flex-col md:text-left text-center md:justify-between max-w-6xl px-8'>
+            <div className="text-and-btn text-slate-100">
+                <p className='text-4xl mb-7'>Turn Your Cursor Into A Colorful Magic Wand & Charm Your Visitors</p>
+                <p className='mb-5'>Motion Art for Elementor is a groundbreaking plugin that empowers you to effortlessly infuse your website with visually stunning motion art elements.</p>
+            <button className='p-3 px-8 rounded-lg bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400  text-slate-900 '>
+                 Purchase here <GoPaperAirplane/>
+            </button>
+            </div>
+            
+            <div className="div px-16 md:mt-0 mt-24">
+                <img src={Magic_stick} alt="" srcset="" />
+            </div>
+        </section>
+        
+            
+        </>
+    )
+}
+
+
+export default Purchase

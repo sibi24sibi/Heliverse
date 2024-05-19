345f6453e25e63888a67b2ef99216ec83185409f
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-05-19 21:01:40 +0530

    smoke is good at every div --fixed btn

--- .history/src/components/Purchase_btn_20240519210106.jsx
+++ .history/src/components/Purchase_btn_20240519210106.jsx
@@ -6,7 +6,7 @@ function Purchase(){
             <div className="text-and-btn text-slate-100">
                 <h1 className='text-3xl mb-7'>Turn Your Cursor Into A Colorful Magic Wand & Charm Your Visitors.</h1>
                 <p className='mb-5 text-slate-500'>Motion Art for Elementor is a groundbreaking plugin that empowers you to effortlessly infuse your website with visually stunning motion art elements.</p>
-            <button className='p-6 px-20  rounded-lg  flex bg-gradient-to-r from-[#F87516] from-10% to-[#5E11FF] to-90%  text-slate-200 '>
+            <button className='p-4 px-24  rounded-lg  flex bg-gradient-to-r from-[#F87516] from-10% to-[#5E11FF] to-90%  text-slate-200 '>
                  <p >Purchase From Evanto  </p><span className='ml-5'> ► </span>
             </button>
             </div>
